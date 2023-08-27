47bcefee7c724f0f264f56e36cdd557ac4c0f35f
Luketss <lucassilluzio@gmail.com>
2023-08-27 14:49:09 -0300

    feature: add enpoint to get model per year

--- fipe/application/main/main.py
+++ fipe/application/main/main.py
@@ -47,3 +47,25 @@ async def post_consultar_modelos(tipo_veiculo, codigo_marca):
     if r.status_code != status.HTTP_200_OK:
         raise HTTPException(status_code=404, detail="Requisição para fipe.org falhou")
     return json.loads(r.text)
+
+
+@app.post("/consultar-modelos-por-ano")
+async def post_consultar_modelos_por_ano(
+    codigo_marca, codigo_modelo, ano, codigo_tipo_combustivel, ano_modelo
+):
+    data = {
+        "codigoTabelaReferencia": "299",
+        "codigoModelo": f"{codigo_modelo}",
+        "codigoMarca": f"{codigo_marca}",
+        "ano": f"{ano}",
+        "codigoTipoCombustivel": f"{codigo_tipo_combustivel}",
+        "anoModelo": f"{ano_modelo}",
+    }
+    r = client.post_request(
+        "https://veiculos.fipe.org.br/api/veiculos/ConsultarModelosAtravesDoAno",
+        payload=data,
+    )
+    print(r)
+    if r.status_code != status.HTTP_200_OK:
+        raise HTTPException(status_code=404, detail="Requisição para fipe.org falhou")
+    return json.loads(r.text)
